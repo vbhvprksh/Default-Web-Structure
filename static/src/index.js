9794dca7e3805217f618f52aef5c2362404033fe
vbhvprksh <vbhvprksh@gmail.com>
2020-09-20 22:37:39 +0530

    final website structure folder

---
+++ static/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <div>
+        <h1>This is JSK!!</h1>
+    </div>,
+    document.getElementById("display")
+)
+
+console.log("hello");
+
+document.write("This is render from Index.js");
---
+++ static/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports={
+    entry:{
+        app:'./src/index.js'
+    },
+    watch:true,
+    devtool:'source-map',
+    output:{
+        filename:'[name].bundle.js',
+        path: path.resolve(__dirname,'dist')
+    },
+    module:{
+        rules:[
+            {
+                test: /\.js$/,
+                exclude:/node_modules/,
+                use: {
+                    loader: "babel-loader"
+                  }
+            }
+        ]
+    },
+    resolve:{
+        extensions:[
+            '.js'
+        ]
+    }
+}
