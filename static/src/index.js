2cf71d05e82cb935fd0d1d5c34eb0577a945d56f
vbhvprksh <vbhvprksh@gmail.com>
2020-10-01 10:46:19 +0530

    forward to newton

--- static/src/index.js
+++ static/src/index.js
@@ -1,13 +1,2 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-ReactDOM.render(
-    <div>
-        <h1>This is JSK!!</h1>
-    </div>,
-    document.getElementById("display")
-)
-
-console.log("hello");
-
-document.write("This is render from Index.js");
